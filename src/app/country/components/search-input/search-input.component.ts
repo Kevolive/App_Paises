e5958a47112ade7063ca7768af9f941d51677421
Kevin Olivella <kevinolivella@gmail.com>
2025-04-08 20:53:38 -0500

    Se agrega el ActivatedRoute, para preservar la información

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input, output, signal } from '@angular/core';
+import { Component, effect, input, linkedSignal, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -8,10 +8,12 @@ import { Component, effect, input, output, signal } from '@angular/core';
 export class SearchInputComponent {
 
 placeholder = input('Buscar')
-value = output<string>();
 debounceTime = input(300);
+inicialValue = input<string>();
 
-inputValue = signal('');
+value = output<string>();
+
+inputValue = linkedSignal<string>(() => this.inicialValue() ?? '');
 
 
 debounceEffetc = effect((onCleanup) => {
